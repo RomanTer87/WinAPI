a86218c9d51f728f78f82af817f3c184b1057961
1 <1@DESKTOP-NU9I6RS>
2023-10-29 23:20:00 +0800

    MainWindow in process

--- MainWindow/main.cpp
+++ MainWindow/main.cpp
@@ -1,6 +1,10 @@
 #include<Windows.h>
 #include"resource.h"
 
+#define IDC_COMBO	1001
+#define IDC_BUTTON_APPLY	1002
+CONST CHAR* g_CURSOR[] = { "Busy.ani", "Normal Select.ani","Working In Background.ani" };
+
 CONST CHAR g_sz_WINDOW_CLASS[] = "My Window Class"; // Имя класса окна
 
 LRESULT CALLBACK WINAPI WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -21,7 +25,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	//wc.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON_RAM));	//Sm - small
 	wc.hIcon = (HICON)LoadImage(hInstance, "Gift.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
 	wc.hIconSm = (HICON)LoadImage(hInstance, "Reward.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
-	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hCursor = (HCURSOR)LoadImage(hInstance, "star.ani", IMAGE_CURSOR, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
 	wc.hbrBackground = HBRUSH(COLOR_WINDOW + 1);
 
 	wc.hInstance = hInstance; // hInstance - это экземпляр исполняемого файла программы в памяти;
@@ -74,8 +78,53 @@ LRESULT CALLBACK WINAPI WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPar
 	switch (uMsg)
 	{
 	case WM_CREATE:
-		break;
+	{
+		HWND hCombo = CreateWindowEx
+		(
+			NULL,
+			"ComboBox",
+			"",
+			WS_CHILD | WS_VISIBLE | CBN_DROPDOWN,
+			10, 10,
+			200, 200,
+			hwnd,
+			(HMENU)IDC_COMBO,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		for (int i = 0;i < sizeof(g_CURSOR) / sizeof(g_CURSOR[0]);i++)
+		{
+			SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)g_CURSOR[i]);
+		}
+
+		HWND hButton = CreateWindowEx
+		(
+			NULL,
+			"Button",
+			"Apply",
+			WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			220, 10,
+			100, 28,
+			hwnd,
+			(HMENU)IDC_BUTTON_APPLY,
+			GetModuleHandle(NULL),
+			NULL
+		);
+	}
+	break;
 	case WM_COMMAND:
+	{
+		switch (LOWORD(wParam))
+		{
+		case IDC_BUTTON_APPLY:
+		{
+			HWND hCombo = GetDlgItem(hwnd, IDC_COMBO);
+			int i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
+			CHAR sz_filename[_MAX_FNAME]{};
+		}
+		break;
+		}
+	}
 		break;
 	case WM_DESTROY:PostQuitMessage(0); break;
 	case WM_CLOSE:  DestroyWindow(hwnd);break;
