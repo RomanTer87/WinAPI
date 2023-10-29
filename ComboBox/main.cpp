6bfd1d9d43b3e0f00006939f07ccb99cda792c1c
1 <1@DESKTOP-NU9I6RS>
2023-10-29 21:01:19 +0800

    COMBOBOX with ADD

--- ComboBox/main.cpp
+++ ComboBox/main.cpp
@@ -6,6 +6,7 @@
 CONST CHAR* g_sz_VALUES[] = { "This", "is", "my", "first", "combo", "box" };
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK DlgProcAdd(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -30,15 +31,19 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
+		case IDC_BUTTON_ADD:
+		{
+			DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD), hwnd, DlgProcAdd, 0);
+		}
 		case IDOK:
 		{
 			CONST INT SIZE = 256;
 			CHAR sz_buffer[SIZE] = {};
 			HWND hCombo = GetDlgItem(hwnd, IDC_COMBO1);
 			int i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
-			SendMessage(hCombo, CB_GETLBTEXT,i,(LPARAM)sz_buffer);
+			SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_buffer);
 			CHAR sz_message[SIZE]{};
-			sprintf(sz_message, " Вы выбрали элемент № %i, со значением \"%s\"",i, sz_buffer);
+			sprintf(sz_message, " Вы выбрали элемент № %i, со значением \"%s\"", i, sz_buffer);
 			// %i - integer
 			// %s - string
 			MessageBox(hwnd, sz_message, "Info", MB_OK | MB_ICONINFORMATION);
@@ -50,5 +55,45 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_CLOSE:EndDialog(hwnd, 0);
 	}
 	return FALSE;
+}
 
+BOOL CALLBACK DlgProcAdd(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		SetFocus(GetDlgItem(hwnd, IDC_EDIT_ADD));
+		break;
+	case WM_COMMAND:
+	{
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_ADD);
+			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+
+			HWND parent = GetParent(hwnd);
+			HWND hCombo = GetDlgItem(parent, IDC_COMBO1);
+			if (SendMessage(hCombo, CB_FINDSTRING, -1, (LPARAM)sz_buffer) == CB_ERR)
+			{
+				if (strlen(sz_buffer) == 0)break;
+				SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)sz_buffer);
+				EndDialog(hwnd, 0);
+			}
+			else
+			{
+				MessageBox(hwnd, "Такое значение уже есть", "Info", MB_OK | MB_ICONINFORMATION);
+			}
+		}
+		break;
+		case IDCANCEL:EndDialog(hwnd, 0);break;
+		}
+	}
+	break;
+	case WM_CLOSE:EndDialog(hwnd, 0);
+	}
+	return FALSE;
 }
